a7efbe71013c125908b6e99b718decb48773dad3
Uzair <109786747+uzairansari0@users.noreply.github.com>
2026-01-21 23:44:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeColor() {
+    const selectElement = document.getElementById("colorSelect");
+
+    if (selectElement.selectedIndex !== -1) {
+        selectElement.remove(selectElement.selectedIndex);
+    }
+}
